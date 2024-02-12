cd2ed7c04780ac5d73e097127d23bebaada57299
buggsboony <boonydeboon@gmail.com>
2024-02-12 14:03:08 -0400

    Humm, bon correction en cours

--- install.sh
+++ install.sh
@@ -13,7 +13,20 @@ sudo ln -s "$PWD/$what$extension" /usr/bin/$what
 echo "done."
 
 
-what="acpihibernate"   
+what="acpi-hibernate"   
+extension=.sh
+#peut être extension vide
+
+echo "Set executable..."
+chmod +x $what$extension
+#echo "lien symbolique vers usr bin"
+sudo ln -s "$PWD/$what$extension" /usr/bin/$what
+echo "done."
+
+
+
+
+what="acpisuspend"   
 extension=.sh
 #peut être extension vide
 
